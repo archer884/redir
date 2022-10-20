dc10e66809fac2835e52ffd55c9a868cc2368187
JMA <archer884@zoho.com>
2022-10-20 18:16:31 -0500

    First cut
    Seems to work?

---
+++ src/args.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+
+#[derive(Clone, Debug, Parser)]
+#[command(version, subcommand_negates_reqs(true))]
+pub struct Args {
+    #[arg(required = true)]
+    key: Option<String>,
+
+    #[command(subcommand)]
+    pub command: Option<Command>,
+}
+
+impl Args {
+    pub fn key(&self) -> &str {
+        self.key.as_deref().unwrap()
+    }
+}
+
+#[derive(Clone, Debug, Parser)]
+pub enum Command {
+    /// Add a mapping expression
+    AddMapping(AddMapping),
+
+    /// Add a redirect
+    AddRedirect(AddRedirect),
+}
+
+#[derive(Clone, Debug, Parser)]
+pub struct AddMapping {
+    /// a regular expression
+    pub expr: String,
+}
+
+#[derive(Clone, Debug, Parser)]
+pub struct AddRedirect {
+    /// a valid key (mappings will be applied)
+    pub from: String,
+    /// a subdirectory name
+    pub to: String,
+}
---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use std::{fs, io, path::PathBuf, process};
+
+mod args;
+mod error;
+
+use args::{AddMapping, AddRedirect, Args, Command};
+use directories::ProjectDirs;
+use hashbrown::HashMap;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+
+type Result<T, E = error::Error> = std::result::Result<T, E>;
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+struct Configuration {
+    /// mappings must be valid regular expressions
+    mappings: Vec<String>,
+    redirects: HashMap<String, String>,
+}
+
+impl Configuration {
+    fn map<'a>(&'a self, key: &'a str) -> &'a str {
+        for mapping in &self.mappings {
+            if let Some(cx) = Regex::new(mapping).unwrap().captures(key) {
+                // For your edification, dear author:
+
+                // We're going to advise users to employ non-capturing groups such that their
+                // first CAPTURING group is the target group, because that's easier than the
+                // rigamarole needed for a named capture group.
+
+                // return cx.name("target").unwrap_or_else(|| cx.get(0).unwrap()).as_str();
+
+                return cx.get(1).unwrap_or_else(|| cx.get(0).unwrap()).as_str();
+            }
+        }
+        key
+    }
+}
+
+fn main() {
+    if let Err(e) = run(&clap::Parser::parse()) {
+        eprintln!("{e}");
+        process::exit(1);
+    }
+}
+
+fn run(args: &Args) -> Result<()> {
+    let mut config = configuration()?;
+
+    if let Some(command) = &args.command {
+        return match command {
+            Command::AddMapping(args) => add_mapping(args, &mut config),
+            Command::AddRedirect(args) => add_redirect(args, &mut config),
+        };
+    }
+
+    let key = config.map(args.key());
+    let redirect = config.redirects.get(key).map(|s| s.as_ref()).unwrap_or(key);
+
+    println!("{redirect}");
+
+    Ok(())
+}
+
+fn add_mapping(args: &AddMapping, config: &mut Configuration) -> Result<()> {
+    let _expr = Regex::new(&args.expr)?;
+    config.mappings.push(args.expr.clone());
+    Ok(write_configuration(config)?)
+}
+
+fn add_redirect(args: &AddRedirect, config: &mut Configuration) -> Result<()> {
+    config.redirects.insert(args.from.clone(), args.to.clone());
+    Ok(write_configuration(config)?)
+}
+
+fn configuration() -> io::Result<Configuration> {
+    let (config_dir, config_file) = get_config_path();
+
+    fs::create_dir_all(&config_dir)?;
+
+    if !config_file.exists() {
+        return Ok(Configuration::default());
+    }
+
+    let config = fs::read_to_string(config_file)?;
+    Ok(serde_json::from_str(&config)?)
+}
+
+fn write_configuration(config: &Configuration) -> io::Result<()> {
+    let (_, path) = get_config_path();
+    let serialized = serde_json::to_string_pretty(&config).unwrap();
+    fs::write(path, &serialized)
+}
+
+fn get_config_path() -> (PathBuf, PathBuf) {
+    let dirs = ProjectDirs::from("org", "Hack Commons", "redir").unwrap();
+    let path = dirs.data_dir();
+    let config_file = path.join("config");
+    (path.into(), config_file)
+}
